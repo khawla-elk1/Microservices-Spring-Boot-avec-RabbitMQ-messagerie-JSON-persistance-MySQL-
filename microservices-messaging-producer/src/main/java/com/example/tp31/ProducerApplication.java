76fa50b3b0e2b2362baaf6e356349ba8cbd5a1fb
Khawla <elkhassibikhawla67@gmail.com>
2025-12-19 20:23:57 +0100

    Initial commit: Microservices EMSI project

---
+++ microservices-messaging-producer/src/main/java/com/example/tp31/ProducerApplication.java
@@ -0,0 +1,14 @@
+package com.example.tp31;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerApplication.class, args);
+    }
+}
